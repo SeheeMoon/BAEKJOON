7745876e173a4e7ff9a495457e27782f812f1ef1
SeheeMoon <anstpgml5035@ajou.ac.kr>
2022-12-20 01:19:17 +0900

    2. 조건문

---
+++ 02_006.py
@@ -0,0 +1,25 @@
+a, b = map(int, input().split())
+c = int(input())
+# if 0 <= a <= 23 and 0 <= b <= 59 and 0 <= c <= 1000:
+#     if b + c < 60:
+#         print(a, b + c)
+#     for i in range (1, 17):
+#         if 60 * i <= b + c < 60 * (i + 1):
+#             if a + i < 24:
+#                 print(a + i, (c - (60 - b)) - 60 * (i - 1))
+#             if a + i >= 24:
+#                 print(a + i - 24, (c - (60 - b)) - 60 * (i - 1))
+
+hour = (b + c)//60
+min = (b + c)%60
+
+if b + c >= 60:
+    if (a + hour) >= 24:
+        a = a - 24
+    a = a + hour
+    print(a, min)
+ 
+else:
+    if (a + hour) >= 24:
+        a = a - 24
+    print(a, b + c)
