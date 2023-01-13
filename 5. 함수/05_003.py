d9ce343ecaeccc340f3f318a12f47640b770a43d
SeheeMoon <anstpgml5035@ajou.ac.kr>
2023-01-13 17:36:57 +0900

    한수

--- 5. 함수/05_003.py
+++ 5. 함수/05_003.py
@@ -5,7 +5,9 @@ for i in range(1, N+1):
         lst.append(i)
     if i // 10 >= 10:
         a = str(i)
-        if int(a[1]) == (int(a[0]) + int(a[2])) / 2:
+        if int(a[1]) == (int(a[0]) + int(a[2])) / 2: # N < 1000 이기 때문에 등차중항을 이용함.
             lst.append(i)
 
 print(len(lst))
+
+# num_list[0]-num_list[1] == num_list[1]-num_list[2] 요렇게 간단하게도 표현 가능하구나
